2f5b4bbcd0b1d4b1ddd6853a68680aaf568d25cc
Faizan Karim <50496663+TheDarkLach@users.noreply.github.com>
2021-01-28 17:03:25 -0800

    Add files via upload

---
+++ bst.h
@@ -0,0 +1,33 @@
+//bst.h
+#include <iostream>
+#include <cstring>
+#include <cctype>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+struct node
+{
+    int data;   //some questions use char * data;
+    node * left;
+    node * right;
+};
+
+class bst
+{
+    public:
+    	//These functions are supplied already
+    	bst();			//supplied
+        void build(); 		//supplied
+        void display(); 	//supplied
+
+/* ************** PLACE YOUR PROTOTYPE HERE ***************** */
+ 
+ 
+ 	private:
+ 		node * root;
+		void display_tree(node*, int);
+};
+  
+
